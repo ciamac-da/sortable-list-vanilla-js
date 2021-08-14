620ff8a6501153439f2565da389e3325c9087c2e
ciamac-da <ciamac.davoudi@gmail.com>
2021-08-14 12:42:39 +0200

    Verified by Marti4n v1.0.3 Scramble List Items

--- index.js
+++ index.js
@@ -20,9 +20,13 @@ const listItems = []
 let dragStartIndex
 
 
-// Index list items into DOM
+// Index list items int o DOM
 const createList = () => {
-[...richestPeople].forEach((person, index)=> {
+[...richestPeople]
+.map(a => ({value: a, sort: Math.random() }))
+.sort((a, b) => a.sort - b.sort)
+.map(a => a.value)
+.forEach((person, index)=> {
  const listItem = document.createElement("li")
  listItem.setAttribute("data-index", index)
  listItem.innerHTML = `
